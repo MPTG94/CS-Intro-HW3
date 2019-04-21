a5208babe48610337ffbec257cc9bd39b2a386f5
Mor Paz <1912.mor@gmail.com>
2019-04-21 03:18:36 +0300

    add exercise skeleton code

--- hw3q1/hw3q1.c
+++ hw3q1/hw3q1.c
@@ -1,25 +1,104 @@
 /*-------------------------------------------------------------------------
-  Include files:
---------------------------------------------------------------------------*/
-
+    Include files:
+-------------------------------------------------------------------------*/
 #include <stdio.h>
-#include <stdlib.h>
-
-
-
-/*=========================================================================
-  Constants and definitions:
-==========================================================================*/
-
-/* put your #defines and typedefs here*/
-
-
 
 /*-------------------------------------------------------------------------
-  The main program. (describe what your program does here)
- -------------------------------------------------------------------------*/
+    Constants and definitions:
+-------------------------------------------------------------------------*/
+#define N 11
+
+/*-------------------------------------------------------------------------
+    Function declaration
+-------------------------------------------------------------------------*/
+void print_welcome();
+void print_enter_board_size();
+void print_board(char board[N][N], int n);
+void print_player_turn(int player_index);
+void print_error();
+void print_winner(int player_index);
+void print_tie();
+
+/*-------------------------------------------------------------------------
+    Implementation
+-------------------------------------------------------------------------*/
 int main()
 {
+    int board_size=0;
+    print_welcome();
+    print_enter_board_size();
+    scanf("%d", &board_size);
 
-  return 0;
+    return 0;
+}
+
+int start_game(int board_size)
+{
+    char board[N][N];
+    for(int i = 0; i < board_size; i++)
+    {
+        for(int j=0; j<board_size; j++)
+        {
+            board[i][j] = '_';
+        }
+    }
+}
+
+//print welcome message
+//1 lines
+void print_welcome()
+{
+    printf("*** Welcome to AVOIDANCE TIC-TAC-TOE game ***\n\n");
+}
+
+//print message to enter board size
+//1 lines
+void print_enter_board_size()
+{
+    printf("Please enter board size (1 to %d):\n", N);
+}
+
+//print the board
+//7 lines
+void print_board(char board[N][N], int n)
+{
+    printf("\nCurrent board:\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("|");
+        for (int j = 0; j < n; j++)
+        {
+            printf("%c|", board[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+//print a request for player with index "player_index" to move
+//1 lines
+void print_player_turn(int player_index)
+{
+    printf("\nPlayer ** %d **, enter next move:\n", player_index);
+}
+
+//print error message
+//1 lines
+void print_error()
+{
+    printf("Illegal move!!!, please try again:\n");
+}
+
+//print the winner
+//1 lines
+void print_winner(int player_index)
+{
+    printf("Player %d Wins! Hooray!\n", player_index);
+}
+
+//print message to announce there is a tie (no one wins)
+//1 lines
+void print_tie()
+{
+    printf("It's a tie!\n");
 }
